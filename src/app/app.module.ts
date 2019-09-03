43b04618d17898c88a172d9a43a1d9a1fb8aa883
Shyam Seshadri <shyamseshadri@gmail.com>
2019-09-03 16:01:47 +0530

    Add skeleton components, services and models for the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ListComponent } from './idea/list/list.component';
+import { ItemComponent } from './idea/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ListComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
